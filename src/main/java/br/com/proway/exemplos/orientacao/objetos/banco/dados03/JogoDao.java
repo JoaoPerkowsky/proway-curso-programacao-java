651b8dbb5e7609680a99b384c8110b222b5adc3b
João Victor Perkowsky de Oliveira <joaoperkowskyoliveira@gmail.com>
2022-08-06 16:43:05 -0300

    Update JogoDao.java

--- src/main/java/br/com/proway/exemplos/orientacao/objetos/banco/dados03/JogoDao.java
+++ src/main/java/br/com/proway/exemplos/orientacao/objetos/banco/dados03/JogoDao.java
@@ -1,5 +1,8 @@
 package br.com.proway.exemplos.orientacao.objetos.banco.dados03;
 
+// DAO: Data Access Object (Representaçao do objeto contido em uma tabela).
 public class JogoDao {
-    
+    private int id;
+    private String nome;
+    private String tipo;
 }
